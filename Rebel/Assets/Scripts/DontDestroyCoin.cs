6fa90dce102bb52a7814c0e280bd542712dc4bae
Momo <mohamedmowafy1999@icloud.com>
2021-01-13 14:48:00 -0500

    Coin counter fix + camera fix

---
+++ Rebel/Assets/Scripts/DontDestroyCoin.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyCoin : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Coin Text");
+        if (obj.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
