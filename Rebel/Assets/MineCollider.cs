67d1bbc592db6351068213ee8a700c107235b930
Momo <mohamedmowafy1999@icloud.com>
2021-01-13 16:43:19 -0500

    Cave enter + collider and background music

---
+++ Rebel/Assets/MineCollider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MineCollider : MonoBehaviour
+{
+    private int currentScene;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene + 1);
+    }
+}
---
+++ Rebel/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        GameObject[] obj = GameObject.FindGameObjectsWithTag("Music");
+        if (obj.Length > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+   
+    }
