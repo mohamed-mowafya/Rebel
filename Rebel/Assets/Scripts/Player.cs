a2beaa5e49bbee42397380eaa9499bc9fea0cc9e
Adriana <adrianagalperin@gmail.com>
2021-01-13 09:32:24 -0500

    Ajustments to merge
    Adriana

--- Rebel/Assets/Scripts/Player.cs
+++ Rebel/Assets/Scripts/Player.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    [Header("Player movement")]
     [SerializeField] private float runSpeed = 3f;
     [SerializeField] private float jumpSpeed = 10f;
+
+    [Header("Sound Effects")]
+    [SerializeField] private AudioClip jumpSound;
+    [SerializeField][Range(0,1)] private float jumpSoundVolume = 0.25f;
+
+
     private Animator myAnimator;
     private Collider2D myCollider2D;
     private bool isAlive = true;
@@ -31,17 +37,26 @@ public class Player : MonoBehaviour
         float controlThrow  =  Input.GetAxis("Horizontal");
         Vector2 playerVelocity = new Vector2(controlThrow * runSpeed ,GetComponent<Rigidbody2D>().velocity.y);
         GetComponent<Rigidbody2D>().velocity = playerVelocity;
-        bool playerHasHorizontalSpeed = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > Mathf.Epsilon; 
-        myAnimator.SetBool("Running",playerHasHorizontalSpeed);
+        bool playerHasHorizontalSpeed = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Running", playerHasHorizontalSpeed);
 
     }
+
+
+    private void PlayJumpSound()
+    {
+        AudioSource.PlayClipAtPoint(jumpSound, Camera.main.transform.position, jumpSoundVolume);
+    }
+
 
     private void Jump()
     {
         if (Input.GetButtonDown("Jump") && myCollider2D.IsTouchingLayers(LayerMask.GetMask("Ground")))
         {
+           
             Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
             GetComponent<Rigidbody2D>().velocity += jumpVelocity;
+            AudioSource.PlayClipAtPoint(jumpSound, Camera.main.transform.position, jumpSoundVolume);
         }
     }
     private void FlipSprite()
---
+++ Rebel/Assets/Spring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spring : MonoBehaviour
+{
+    [SerializeField] private float jumpSpeed = 10f;
+    [SerializeField] private Player player;
+    private Animator myAnimator;
+
+    private void Start()
+    {
+        myAnimator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Vector2 jumpVelocity = new Vector2(0f, jumpSpeed);
+        player.GetComponent<Rigidbody2D>().velocity +=jumpVelocity;
+        myAnimator.SetBool("Spring", true);
+        StartCoroutine(ExitAnimation());
+    }
+
+    IEnumerator ExitAnimation()
+    {
+        yield return new WaitForSeconds(0.5f);
+        myAnimator.SetBool("Spring", false);
+    }
+    
+}
